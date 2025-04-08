7d54432806caabb24d0484c9b91d36d8178eca0b
DAIKI <daiki.evilone@gmail.com>
2025-04-08 20:45:10 +0900

    fix camera

--- UniKart/Assets/UniKart/Scripts/Runtime/KartCamera.cs
+++ UniKart/Assets/UniKart/Scripts/Runtime/KartCamera.cs
@@ -10,19 +10,35 @@ namespace UniKart
 
         public Vector3 OffsetRotation = new Vector3(0f, 0f, 0f);
 
+        public float ChaseDistance = 1f;
+
         public float TiltRatio = 0.5f;
+
+        private Vector3 _pivot;
+
+        private void Start()
+        {
+            _pivot = Target.position - Target.forward * ChaseDistance;
+        }
 
         private void LateUpdate()
         {
             var groundNormal = Vector3.up;
             var lastCameraPosition = transform.position;
 
-            var camToKart = Target.position - lastCameraPosition;
-            camToKart.y = 0f;
-            var horizontalRot = Quaternion.LookRotation(camToKart);
+            _pivot = Target.position + (_pivot - Target.position).normalized * ChaseDistance;
 
-            transform.rotation = horizontalRot * Quaternion.Euler(OffsetRotation);
-            transform.position = Target.position + horizontalRot * OffsetPosition;
+            // Horizontal rotation
+            var camToKart = Target.position - _pivot;
+            var horizontalCamToKart = camToKart - Vector3.Project(camToKart, groundNormal);
+            var rot = Quaternion.LookRotation(horizontalCamToKart);
+
+            // Tilt rotation
+            var tiltRot = Quaternion.LookRotation(new Vector3(0, Vector3.Dot(camToKart, groundNormal), horizontalCamToKart.magnitude));
+            rot = Quaternion.Slerp(rot, rot * tiltRot, TiltRatio);
+
+            transform.rotation = rot * Quaternion.Euler(OffsetRotation);
+            transform.position = Target.position + rot * OffsetPosition;
         }
     }
 }
